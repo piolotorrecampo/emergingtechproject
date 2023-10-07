20c99b47a387189b854df627dc96ecbd57a51d19
qjpstorrecampo <qjpstorrecamp@tip.edu.ph>
2023-10-07 20:56:43 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Mekus Mekus</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
