b1935593f5d7b3b8e3284b66077812fd8bc7b71a
qjpstorrecampo <qjpstorrecamp@tip.edu.ph>
2023-10-07 21:50:27 +0800

    landing page

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LandingPage from './screens/LandingPage';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Mekus Mekus</Text>
+      <LandingPage/>
     </View>
   );
 }
---
+++ screens/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, ImageBackground, StyleSheet } from 'react-native'
+
+const LandingPage = () => {
+  return (
+    <View>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        
+      </ImageBackground>
+    </View>
+  )
+}
+
+export default LandingPage
